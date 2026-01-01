3e6cfa44d7d443a4e0af3f43a288d06346663d48
kaptan.vit <kaptan.vit@gmail.com>
2026-01-01 22:38:03 +0530

    Update variable.tf

--- module/azurerm_keyvault/variable.tf
+++ module/azurerm_keyvault/variable.tf
@@ -1,13 +1,19 @@
 variable "kv_todo" {
-    description = "key vault configuration"
-    type = map(object({
-        kv_name = string
-        location = string
-        rg_name = string
-        enabled_for_disk_encryption = bool
-        soft_delete_retention_days = number
-        purge_protection_enabled = bool
-        sku_name = string
-    }))
-  
+  description = "key vault configuration"
+  type = map(object({
+    kv_name                     = string
+    location                    = string
+    rg_name                     = string
+    enabled_for_disk_encryption = bool
+    soft_delete_retention_days  = number
+    purge_protection_enabled    = bool
+    sku_name                    = string
+    network_acls = object({
+      default_action             = string
+      bypass                     = string
+      ip_rules                   = list(string)
+      virtual_network_subnet_ids = list(string)
+    })
+  }))
+
 }
